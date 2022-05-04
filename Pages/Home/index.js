2b1f536d26b6bd9e854dd3de99edd09ae4d15fc0
Matheus Dresch <matheus_dresch@sicredi.com.br>
2022-05-04 17:15:28 -0300

    Refazendo home inteira usando bootstrap: header e inicio finalizados

--- Pages/Home/index.js
+++
@@ -1,6 +0,0 @@
-const mobileMenuButton = document.querySelector('.mobile-menu-button')
-const menuLateral = document.querySelector('.mobile-menu')
-
-mobileMenuButton.addEventListener('click', () => {
-    menuLateral.classList.toggle('mobile-menu--visible')
-})
