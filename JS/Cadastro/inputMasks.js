6ac55538d2c2775dd71ec25319726c7b59e2a6cb
Matheus Dresch <matheus_dresch@sicredi.com.br>
2022-05-05 22:33:32 -0300

    Toda estrutura de arquivos reorganizada, pagina home finalizada.

---
+++ JS/Cadastro/inputMasks.js
@@ -0,0 +1,31 @@
+function phoneMask(input) {
+    let phone = input.value
+
+    phone = phone.replace(/\D/g, "");
+    phone = phone.replace(/^(\d{2})(\d)/, "($1) $2");
+    phone = phone.replace(/\s(\d{1})/, " $1 ")
+    phone = phone.replace(/(\d)(\d{4})$/, "$1-$2");
+
+    input.value = phone
+}
+
+function cpfMask(input = '') {
+    let cpf = input.value
+
+    cpf = cpf.replace(/\D/g, '')
+
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2')
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+
+    input.value = cpf
+}
+
+function cepMask(input) {
+    let cep = input.value
+
+    cep = cep.replace(/\D/g, '')
+    cep = cep.replace(/(\d{5})(\d{3})/, '$1-$2')
+
+    input.value = cep
+}
