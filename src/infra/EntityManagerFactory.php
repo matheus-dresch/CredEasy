e79a8d1101721e9fd0562fc3038857efe878703a
Matheus Dresch <matheus_dresch@sicredi.com.br>
2022-07-06 16:10:23 -0300

    Reorganizar as pastas de acordo com o modelo MVC

---
+++ src/infra/EntityManagerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMSetup;
+
+class EntityManagerFactory
+{
+    public static function GetEntityManager(): EntityManagerInterface
+    {
+        $rootDir = __DIR__ . '/..';
+        $paths = [$rootDir . '/src/entity'];
+
+        $config = ORMSetup::createAnnotationMetadataConfiguration($paths);
+
+        $driver = [
+            'driver' => 'pdo_mysql',
+            'host' => '127.0.0.1',
+            'dbname' => 'credeasy',
+            'user' => 'root',
+            'password' => 'root'
+        ];
+
+        return EntityManager::create($driver, $config);
+    }
+}
